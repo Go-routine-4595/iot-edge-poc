4646cee4cb672a47f0040897a33d283cb3df243d
Go-routine-4595 <christophe.buffard@gmail.com>
2026-08-15 18:15:06 -0700

    ekuiper: - init script added comments (volume mapping in the containter) - rule update, filter event with confidence > 0.6

--- ekuiper/init.sh
+++ ekuiper/init.sh
@@ -5,6 +5,10 @@ until curl -fsS "$BASE/streams" >/dev/null 2>&1; do
   sleep 1
 done
 
+# we mount the ekuiper-init docker with this volume:
+#      - ./ekuiper/stream.json:/config/stream.json:ro
+#      - ./ekuiper/rule.json:/config/rule.json:ro
+
 curl -fsS "$BASE/streams" | grep -q '"inference_events"' ||   curl -fsS -X POST -H 'Content-Type: application/json' --data-binary @/config/stream.json "$BASE/streams"
 
 curl -fsS "$BASE/rules" | grep -q '"id":"create_alert"' ||   curl -fsS -X POST -H 'Content-Type: application/json' --data-binary @/config/rule.json "$BASE/rules"
